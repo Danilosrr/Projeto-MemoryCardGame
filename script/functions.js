f3d74216b962f4b88405daf1c56e01963a173d70
DaniloRibeiro <danilo_srr@hotmail.com>
2022-02-01 22:30:43 -0300

    added card sorting

--- script/functions.js
+++ script/functions.js
@@ -1,15 +1,17 @@
+cardList=[];
 let cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
 while(cardNumber%2 != 0 || cardNumber>14 || cardNumber<4){
 cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
 cardNumber=parseInt(cardNumber);
-}
-
-for(let i=1; i<=cardNumber; i++){
- document.querySelector(".hide").classList.remove("hide");
 };
 
-const cards=document.querySelectorAll('.card');
-cards.forEach(card => card.addEventListener('click',flip));
+
+for(let i=0; i<cardNumber; i++){
+cardList[i]=document.querySelector(".hide");
+document.querySelector(".hide").addEventListener('click',flip)
+document.querySelector(".hide").style.order=Math.floor(Math.random() * 10);
+document.querySelector(".hide").classList.remove("hide");
+};
 
 let firstflip=true;
 let firstCard;
@@ -28,11 +30,11 @@ function flip(){
         }; 
         
     matchCheck();
-}
+};
 
 function matchCheck(){
 
-    if(firstCard.dataset.framework === secondCard.dataset.framework){
+    if(firstCard.dataset.framework === secondCard.dataset.framework && firstCard != secondCard){
         firstCard.removeEventListener('click',flip);
         secondCard.removeEventListener('click',flip);
         firstCard=undefined;
@@ -43,7 +45,6 @@ function matchCheck(){
         secondCard.classList.remove('flip');
         firstCard=undefined;
         secondCard=undefined;
-    }, 1500);
-}
+        }, 1000);
+    }
 };
-
