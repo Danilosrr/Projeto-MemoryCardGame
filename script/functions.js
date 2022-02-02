dd38b07908479549a00c877b430d74884406148d
DaniloRibeiro <danilo_srr@hotmail.com>
2022-02-02 00:39:12 -0300

    dinamic card creation added, sorting changed

--- script/functions.js
+++ script/functions.js
@@ -1,26 +1,51 @@
-cardList=[];
+/* Card Number Input */
+ParrotPairs=["bobross","bobross", "explody","explody", "fiesta","fiesta","metal","metal", "revertit","revertit", "triplets","triplets","unicorn","unicorn"]
 let cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
 while(cardNumber%2 != 0 || cardNumber>14 || cardNumber<4){
 cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
 cardNumber=parseInt(cardNumber);
 };
 
+/* Sort Cards */
+
+ParrotSorted=ParrotPairs.slice(0,(cardNumber-14));
+ParrotSorted.sort(comparador);
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+/* Dinamically Create Inputs */
 
 for(let i=0; i<cardNumber; i++){
-cardList[i]=document.querySelector(".hide");
-document.querySelector(".hide").addEventListener('click',flip)
-document.querySelector(".hide").style.order=Math.floor(Math.random() * 10);
-document.querySelector(".hide").classList.remove("hide");
+
+    let Cards = document.createElement('div');
+    Cards.setAttribute('class', 'card');
+    Cards.setAttribute('onclick','flip(this)')
+    Cards.setAttribute('data-identifier', 'card');
+    Cards.innerHTML += `
+                <div class="front-face face" identifier="front-face">
+                    <img src="./imgs/front 7.png" alt="Parrot">
+                </div>
+
+                <div class="back-face face" identifier="back-face">
+                    <img src="./imgs/${ParrotSorted[i]}parrot.gif">
+                </div>
+            `;
+
+    document.querySelector('main').appendChild(Cards);
+
 };
+
+/* yet to change */
 
 let firstflip=true;
 let firstCard;
 let secondCard;
 
 
-function flip(){
+function flip(selected){
     
-    this.classList.toggle('flip');
+    selected.classList.toggle('flip');
 
     if(firstflip==true){
         firstflip=false;
@@ -30,6 +55,7 @@ function flip(){
         }; 
         
     matchCheck();
+    winCheck();
 };
 
 function matchCheck(){
@@ -37,6 +63,7 @@ function matchCheck(){
     if(firstCard.dataset.framework === secondCard.dataset.framework && firstCard != secondCard){
         firstCard.removeEventListener('click',flip);
         secondCard.removeEventListener('click',flip);
+        pairsNumber += 1
         firstCard=undefined;
         secondCard=undefined;
     }else{
@@ -46,5 +73,11 @@ function matchCheck(){
         firstCard=undefined;
         secondCard=undefined;
         }, 1000);
+    }
+};
+
+function winCheck(){
+    if(pairsNumber==(cardNumber/2)){
+        alert('Você Ganhou!');
     }
 };
