af78e93724c4e3e73463bb098878a51eb786bd97
DaniloRibeiro <danilo_srr@hotmail.com>
2022-02-02 22:12:41 -0300

    reconstructing code to acomodate previous changes

--- script/functions.js
+++ script/functions.js
@@ -1,14 +1,18 @@
+var cardsFlipped = [];
+
 /* Card Number Input */
-ParrotPairs=["bobross","bobross", "explody","explody", "fiesta","fiesta","metal","metal", "revertit","revertit", "triplets","triplets","unicorn","unicorn"]
+
 let cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
+
 while(cardNumber%2 != 0 || cardNumber>14 || cardNumber<4){
 cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
 cardNumber=parseInt(cardNumber);
 };
 
 /* Sort Cards */
-
-ParrotSorted=ParrotPairs.slice(0,(cardNumber-14));
+ParrotPairs=["bobross", "explody", "fiesta","metal","revertit", "triplets", "unicorn"]
+ParrotSorted=ParrotPairs.slice(0,cardNumber/2);
+ParrotSorted=ParrotSorted.concat(ParrotSorted);
 ParrotSorted.sort(comparador);
 function comparador() { 
 	return Math.random() - 0.5; 
@@ -20,7 +24,7 @@ for(let i=0; i<cardNumber; i++){
 
     let Cards = document.createElement('div');
     Cards.setAttribute('class', 'card');
-    Cards.setAttribute('onclick','flip(this)')
+    Cards.setAttribute('onclick','selecionar(this)')
     Cards.setAttribute('data-identifier', 'card');
     Cards.innerHTML += `
                 <div class="front-face face" identifier="front-face">
@@ -35,49 +39,26 @@ for(let i=0; i<cardNumber; i++){
     document.querySelector('main').appendChild(Cards);
 
 };
-
+/* Flip cards */
+function flipCard(element) {
+    let back= element.querySelector(".back-face");
+    let front= element.querySelector(".front-face");
+    back.classList.toggle("back-face-rotate");
+    front.classList.toggle("front-face-rotate");
+}
+  
 /* yet to change */
 
-let firstflip=true;
-let firstCard;
-let secondCard;
-
-
-function flip(selected){
+function selecionar(cartaSelecionada) {
     
-    selected.classList.toggle('flip');
+  if (cardsFlipped.length == 0) {
+    cardsFlipped.push(cartaSelecionada);
+    flipCard(cartaSelecionada);
 
-    if(firstflip==true){
-        firstflip=false;
-        firstCard=this;}else{
-        secondCard=this;
-        firstflip=true;
-        }; 
-        
-    matchCheck();
-    winCheck();
-};
-
-function matchCheck(){
-
-    if(firstCard.dataset.framework === secondCard.dataset.framework && firstCard != secondCard){
-        firstCard.removeEventListener('click',flip);
-        secondCard.removeEventListener('click',flip);
-        pairsNumber += 1
-        firstCard=undefined;
-        secondCard=undefined;
-    }else{
-    setTimeout(()=>{
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-        firstCard=undefined;
-        secondCard=undefined;
-        }, 1000);
-    }
-};
-
-function winCheck(){
-    if(pairsNumber==(cardNumber/2)){
-        alert('Você Ganhou!');
-    }
-};
+  }else if (cardsFlipped.length == 1){
+    flipCard(cartaSelecionada);
+    cardsFlipped.push(cartaSelecionada);
+    testarigualdade;
+    cardsFlipped=[];}
+}
+ 
