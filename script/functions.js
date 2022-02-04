cf281cd6d4fb15faf2823695ffce5794493dc604
DaniloRibeiro <danilo_srr@hotmail.com>
2022-02-04 01:00:47 -0300

    timer added, reset option added

--- script/functions.js
+++ script/functions.js
@@ -1,120 +1,145 @@
 let cardsFlipped = [];
 let cardsSelected=[]
 let igualdade=null
-let repeticao=null
 let pairsFound=0
-let plays=0
-let blockSelection=false
+let enabled=true
 let first
 let second
+let cardNumber
+let playing=false
+ParrotPairs=["bobross", "explody", "fiesta","metal","revertit", "triplets", "unicorn"]
+const plays=document.querySelector('#plays-counter')
+const clock=document.querySelector('#time-countdown')
+
+/*Game Script */
+startGame();
+function startGame(){
+    clearInterval(countdown,1000);
+    resetValues();
+    cardsInput();
+    sortCards();
+    createCards();
+    count = setInterval(countdown,1000);
+
+}
+function resetValues(){
+    cardsFlipped = [];
+    cardsSelected=[]
+    igualdade=null
+    pairsFound=0
+    enabled=true
+    first=undefined
+    second=undefined
+    playing=false
+    clock.innerHTML = 0;
+    plays.innerHTML = 0;
+}
+
 /* Card Number Input */
+function cardsInput(){
+    cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
 
-let cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
+    while(cardNumber%2 != 0 || cardNumber>14 || cardNumber<4){
+    cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
+    cardNumber=parseInt(cardNumber);
+    }
 
-while(cardNumber%2 != 0 || cardNumber>14 || cardNumber<4){
-cardNumber=prompt("Quantas cartas voce deseja?(números pares de 4 á 14)");
-cardNumber=parseInt(cardNumber);
-};
+}
 
 /* Sort Cards */
-ParrotPairs=["bobross", "explody", "fiesta","metal","revertit", "triplets", "unicorn"]
-ParrotSorted=ParrotPairs.slice(0,cardNumber/2);
-ParrotSorted=ParrotSorted.concat(ParrotSorted);
-ParrotSorted.sort(comparador);
+function sortCards(){
+    ParrotSorted=ParrotPairs.slice(0,cardNumber/2);
+    ParrotSorted=ParrotSorted.concat(ParrotSorted);
+    ParrotSorted.sort(comparador);
+}
+
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-/* Dinamically Create Inputs */
+/* Dinamically Create Cards */
+function createCards(){
 
-for(let i=0; i<cardNumber; i++){
+    for(let i=0; i<cardNumber; i++){
 
-    let Cards = document.createElement('div');
-    Cards.setAttribute('class', 'card card'+i);
-    Cards.setAttribute('data-id', ParrotSorted[i]);
-    Cards.setAttribute('onclick','selecionar(this)')
-    Cards.setAttribute('data-identifier', 'card');
-    Cards.innerHTML += `
-                <div class="front-face face" identifier="front-face">
-                    <img src="./imgs/front 7.png" alt="Parrot">
-                </div>
+        let Cards = document.createElement('div');
+        Cards.setAttribute('class', 'card card'+i);
+        Cards.setAttribute('data-id', ParrotSorted[i]);
+        Cards.setAttribute('onclick','chooseCard(this)')
+        Cards.setAttribute('data-identifier', 'card');
+        Cards.innerHTML += `
+                    <div class="front-face face" identifier="front-face">
+                        <img src="./imgs/front 7.png" alt="Parrot">
+                    </div>
 
-                <div class="back-face face" identifier="back-face">
-                    <img src="./imgs/${ParrotSorted[i]}parrot.gif">
-                </div>
-            `;
+                    <div class="back-face face" identifier="back-face">
+                        <img src="./imgs/${ParrotSorted[i]}parrot.gif" alt="Funny parrot">
+                    </div>
+                `;
 
-    document.querySelector('main').appendChild(Cards);
+        document.querySelector('main').appendChild(Cards);
+        playing=true;
+    };
+}
 
-};
-/* Flip cards */
-
+/* Flip Cards */
 function flipCard(element) {
 
     
     let back= element.querySelector(".back-face");
     let front= element.querySelector(".front-face");
-    back.classList.add("back-face-rotate");
-    front.classList.add("front-face-rotate");
+    back.classList.toggle("back-face-rotate");
+    front.classList.toggle("front-face-rotate");
 
 }
 
-function unflipCard(element) {
-
-    
-    let back= element.querySelector(".back-face");
-    let front= element.querySelector(".front-face");
-    back.classList.remove("back-face-rotate");
-    front.classList.remove("front-face-rotate");
-
-}
-   
-
-/* seleciona a carta */
-function selecionar(cartaSelecionada) {
- 
-    
-    if(cardsFlipped.includes(cartaSelecionada) == false){
-    
-
+/* Choose Card */
+function chooseCard(cardChosen) {
+    if(cardsFlipped.includes(cardChosen) == false){
+        if(!enabled) return
+        enabled = false
 
         if (cardsSelected.length==0) {
-            /*selecionar primeira carta*/
-            first=cartaSelecionada;
-            flipCard(cartaSelecionada);
-            cardsSelected.push(cartaSelecionada);
+
+            /*First Card*/
+            first=cardChosen;
+            flipCard(cardChosen);
+            cardsSelected.push(cardChosen);
             cardsFlipped.push(first);
-            plays++
-            console.log('if')
+            plays.innerHTML = parseInt(plays.innerHTML) + 1;
+            enabled = true;
+
         } 
         else if (cardsSelected.length==1) 
         {
-            /*selecionar segunda carta*/
-            second=cartaSelecionada;
-            flipCard(second);
-            plays++
-
-            testarIgualdade(first,second);
-            console.log('else if') 
-
+            /*Second Card*/
+            second=cardChosen;
+            testEqual(first,second);
+            
             if (igualdade===true){
-
+                
+                plays.innerHTML = parseInt(plays.innerHTML) + 1;
+                flipCard(second);
                 cardsFlipped.push(second);
                 cardsSelected.pop();
-                pairsFound++;
-                console.log('else if if')
+                pairsFound++
+                enabled = true;
+
             }
 
-           
             if (igualdade===false){
 
+                plays.innerHTML = parseInt(plays.innerHTML) + 1;
+                flipCard(second);
                 setTimeout(() => {
-                unflipCard(second);
-                unflipCard(first);
-                cardsSelected.pop();
-                cardsFlipped.pop();
-                },500);
-                console.log('else if if2')
+
+                    flipCard(second);
+                    flipCard(first);
+                    cardsSelected.pop();
+                    cardsFlipped.pop();
+                    enabled = true;
+                },1000);
+
             }
 
         }
@@ -124,7 +149,8 @@ function selecionar(cartaSelecionada) {
 
 }
 
-function testarIgualdade(item1,item2){
+/* Test for equal card, same card, and winning */
+function testEqual(item1,item2){
     
     if (item1.getAttribute('data-id')===item2.getAttribute('data-id') && item1.getAttribute('class') !== item2.getAttribute('class')){
         igualdade=true;
@@ -132,14 +158,33 @@ function testarIgualdade(item1,item2){
 
 }
 
-function testarRepeticao(item1,item2){
+function winCondition(){
 
-    if (item1.getAttribute('data-id')===item2.getAttribute('data-id') && item1.getAttribute('class') === item2.getAttribute('class')){
-        repeticao=true;
-    }else{repeticao=false;}
-
+    if(pairsFound==cardNumber/2){
+        setTimeout(()=>{
+        alert(`Você Ganhou em ${plays.innerHTML} jogadas e ${clock.innerHTML} segundos!`)
+        playing=false
+        clearInterval(countdown)
+        reset=prompt('Você quer jogar denovo? s / n')
+        while(reset != 's' && reset != 'n'){
+            reset=prompt("Você quer jogar denovo? digite s para sim ou n para não");
+            }
+        if(reset=='n'){
+            return;
+        }
+        if (reset=='s'){
+            document.querySelector('main').innerHTML="";
+            clearInterval(count);
+            startGame();
+        }
+        },500);
+    }
 }
 
-function winCondition(){
-    if(pairsFound==cardNumber/2){alert(`Voce Ganhou! em ${plays} jogadas`)}
+/* Timer and plays*/
+function countdown(){
+
+    if (pairsFound < cardNumber && playing==true) {
+        clock.innerHTML = parseInt(clock.innerHTML) + 1;
+    }
 }
